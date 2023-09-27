fbcb58115107096189ab33bcbe9fea221aac365b
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-27 19:41:19 +0300

    Create purchases pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#hello'
   get 'pages/hello'
-  resources :categories
+  resources :categories do
+    resources :purchases, only: [:new, :create]
+  end
+  resources :purchases, except: [:new, :show, :index]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
