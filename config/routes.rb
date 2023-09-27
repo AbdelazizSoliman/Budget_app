8a86c724fe0d3da3e1a33a3698001b3082b6f9ed
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-27 16:08:14 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#hello'
+  get 'pages/hello'
+  resources :categories
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
